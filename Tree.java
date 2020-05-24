361456b7933ad3c3b82571b2a236dbb18f9c9f18
Abhay Singh Shekhawat <AbhiTheCoder@users.noreply.github.com>
2020-05-24 16:51:11 +0530

    Other Treversals

--- Tree.java
+++ Tree.java
@@ -1,20 +1,54 @@
 package AbhiSir;
 
 import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
 
+//Binary
+//n-ary tree
 public class Tree {
 //BFS -
 //->level order traversal
 //DFS -
 //->Inorder,preorder,postorder
 
-//                   1
-//            2              3
+//level =3
+//                   1 ->null
+//            2              3 ->null
 //
-//  4           5        6      7
+//  4           5        6      7 -> null
 
-//          8       9
+//          8       5
 
+    
+    //level order
+    //queue -    8 9 null
+    //output - 1 2 3 4 5 6 7 8 9
+    /*while(q.size()>1){
+        TreeNode t = q.poll();
+        if(t ==null)
+        q.add(t);
+        else{
+        if(left/right ! null)
+        q.add(left/right);
+        sout(t.val);
+        }
+    }
+    
+    q.add(new Pair(0,1));
+    Pair p = q.poll();
+    int x =p.x;
+    int y =p.y;
+     visited[x][y] == 0
+     
+    class Pair{
+        int x;int y;
+        public Pair(int x,int y){
+            this.x =x;
+            this.y =y;
+        }
+    }
+    * */
 //stack -
 //print - 4 8 9 5 2 6 7 3 1
 //l->node->r - 4 2 5 1 6 3 7
@@ -28,6 +62,29 @@ public class Tree {
          public TreeNode(int val){
              this.val=val;
          }
+    }
+    public void levelOrderTraversal(TreeNode root) {
+        if(root==null)
+            return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        while(queue.size()>1){
+            TreeNode node  = queue.poll();
+            if(node ==null){
+                //remove from first and add null at last
+                queue.add(node);
+            }else{
+                //check left & right for null and push
+                if(node.left!=null)
+                    queue.add(node.left);
+                if(node.right!=null)
+                    queue.add(node.right);
+
+                //print
+                System.out.println(node.val);
+            }
+        }
     }
     public void inorder(TreeNode root) {
         if(root==null)
@@ -65,3 +122,15 @@ public class Tree {
         t.inorder(root);
     }
 }
+// -> l,r,u,d
+//[-1   0  -1 -1
+// -1  -1  -1 -1
+//  0  -1   0 -1] - BFS
+//visited - 0,0 null
+//queue = 0,1 , 1,0 null 
+//
+//
+//        [-1 0 -1 -1
+//         0 -1 -1 -1
+//         0 -1  0 -1
+//        ] 
